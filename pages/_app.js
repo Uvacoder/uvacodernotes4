283b4057cd650602778bdee52c5c55ec68bb82bb
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-04-08 21:01:48 +0700

    chore: implement google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+import * as gtag from '../lib/gtag'
+
 import 'nextra-theme-docs/style.css'
 
-export default function Nextra({ Component, pageProps }) {
+const Nextra = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
+
+export default Nextra
