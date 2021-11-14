71a3c35cfd88152fbcebaaebfa2abba71488cd05
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-11-14 11:30:58 +0700

    chore: disable analytics for local dev

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 import { UMAMI_SRC, UMAMI_WEBSITE_ID } from '../constants/umami'
+import { isDev } from '../constants/configs'
 
 export default class MyDocument extends Document {
   render() {
@@ -8,12 +9,14 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           {/* umami self-hosted analytics */}
-          <script
-            async
-            defer
-            data-website-id={UMAMI_WEBSITE_ID}
-            src={UMAMI_SRC}
-          />
+          {!isDev && (
+            <script
+              async
+              defer
+              data-website-id={UMAMI_WEBSITE_ID}
+              src={UMAMI_SRC}
+            />
+          )}
         </Head>
         <body>
           <Main />
